2971e71e3895ca4bcf6ad4a7bd91e3abb498ec36
Andrey Shpak PC <andrey.shpak.it@gmail.com>
2021-09-06 21:49:40 +0300

    init repo

---
+++ AppServer/Models/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace AppServer.Models
+{
+    public class Card
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUri { get; set; }
+    }
+}
